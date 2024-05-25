b5d33efd51da811baa1ce7fd4d4fc59414f89ba0
rijan <rijanadhikari@gmail.com>
2024-05-25 15:28:55 +0900

    adde

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NuxtWelcome />
+    <SfButton class="w-full"> Hello </SfButton>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { SfButton } from '@storefront-ui/vue';
+</script>
+
